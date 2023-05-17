ab27e476e5a78aab8b9186d8669aeefdcc6ab82a
SWE - Wid Seraj <120143921+wid21@users.noreply.github.com>
2023-05-18 01:37:58 +0300

    Add files via upload

---
+++ SpringBoot-HW14/src/main/java/com/example/springboothw14/ApiResponse/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.example.springboothw14.ApiResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+    private String message;
+}
